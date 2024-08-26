4c891ff50b18bd5aef4d8e55475594a559cf4cc1
PrathameshSonje <prathameshsonje147@gmail.com>
2024-08-26 23:56:05 +0530

    used infinite queries

--- src/components/chat/chatMessage.tsx
+++ src/components/chat/chatMessage.tsx
@@ -1,23 +1,53 @@
+import { trpc } from "@/app/_trpc/client"
 import { Message } from "./message"
+import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query"
+import { useContext } from "react"
+import { ChatContext } from "./ChatContext"
+import { Loader2 } from "lucide-react"
 
-export const ChatMessage = () => {
+interface MessageProps {
+    fileId: string
+}
 
-    const isUserMessage = true;
+export const ChatMessage = ({ fileId }: MessageProps) => {
+
+    const { isLoading: isAiThinking } =
+        useContext(ChatContext)
+
+    const { data, isLoading, fetchNextPage } =
+        trpc.getFileMessages.useInfiniteQuery(
+            {
+                fileId,
+                limit: INFINITE_QUERY_LIMIT,
+            },
+            {
+                getNextPageParam: (lastPage) => lastPage?.nextCursor,
+            }
+        )
+
+    const loadingMessage = {
+        createdAt: new Date().toISOString(),
+        id: 'loading-message',
+        isUserMessage: false,
+        text: (
+            <span className='flex h-full items-center justify-center'>
+                <Loader2 className='h-4 w-4 animate-spin' />
+            </span>
+        ),
+    }
+
+    const messages = data?.pages.flatMap(
+        (page) => page.messages
+    )
+
+    const combinedMessages = [
+        ...(isAiThinking ? [loadingMessage] : []),
+        ...(messages ?? []),
+    ]
 
     return (
         <div className="h-full flex flex-col gap-8">
-            <Message message="hello there" isUserMessage={isUserMessage} />
-            <Message message="hello there" isUserMessage={!isUserMessage} />
-            <Message message="hello there" isUserMessage={isUserMessage} />
-            <Message message="hello there" isUserMessage={!isUserMessage} />
-            <Message message="hello there" isUserMessage={isUserMessage} />
-            <Message message="hello there" isUserMessage={!isUserMessage} />
-            <Message message="hello there" isUserMessage={isUserMessage} />
-            <Message message="hello there" isUserMessage={!isUserMessage} />
-            <Message message="hello there" isUserMessage={isUserMessage} />
-            <Message message="hello there" isUserMessage={!isUserMessage} />
-            <Message message="hello there" isUserMessage={isUserMessage} />
-            <Message message="hello there" isUserMessage={!isUserMessage} />
+            <Message message="hello there" isUserMessage={combinedMessages[0].isUserMessage} />
         </div>
     )
 }
--- src/components/chat/chatWrapper.tsx
+++ src/components/chat/chatWrapper.tsx
@@ -94,7 +94,7 @@ const ChatWrapper = ({ fileId }: { fileId: string }) => {
         <ChatContextProvider fileId={fileId}>
             <div className="pl-4 py-4 pr-[2px] flex flex-col h-full ">
                 <div className="h-full max-h-[calc(100vh-3.5rem-6rem)] overflow-y-auto scrollbar-thumb-red scrollbar-thumb-rounded scrollbar-track-red-lighter scrollbar-w-2 pr-3">
-                    <ChatMessage />
+                    <ChatMessage fileId={fileId} />
                 </div>
                 <div className="w-full mt-4 flex items-end">
                     <ChatInput />
