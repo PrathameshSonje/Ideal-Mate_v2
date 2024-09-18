7a22114d8f0d2534b2615b268adaf6d0ecd331b7
PrathameshSonje <prathameshsonje147@gmail.com>
2024-09-18 11:07:37 +0530

    minor changes

--- src/lib/others/pinecone.ts
+++ src/lib/others/pinecone.ts
@@ -3,6 +3,7 @@ import { Pinecone } from '@pinecone-database/pinecone';
 export const getPineconeClient = async () => {
     const client = new Pinecone({
         apiKey: process.env.PINECONE_API_KEY!,
+        fetchApi: fetch
     })
 
     return client
